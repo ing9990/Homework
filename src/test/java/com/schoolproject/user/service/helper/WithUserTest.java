e362050517a351a1e7202d1334a124738bf684b3
TaeWoo <gimgau0218@aiaracorp.com>
2022-08-30 02:19:19 +0900

    feat: Add test code helper class

---
+++ src/test/java/com/schoolproject/user/service/helper/WithUserTest.java
@@ -0,0 +1,70 @@
+package com.schoolproject.user.service.helper;
+
+/**
+ * @author Taewoo
+ */
+
+
+import com.schoolproject.user.domain.School;
+import com.schoolproject.user.repository.SchoolRepository;
+import com.schoolproject.user.repository.UserRepository;
+import com.schoolproject.user.service.SchoolService;
+import com.schoolproject.user.service.UserSecurityService;
+import com.schoolproject.user.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+
+import java.util.*;
+
+public class WithUserTest {
+
+    @Autowired
+    protected SchoolRepository schoolRepository;
+
+    @Autowired
+    protected UserRepository userRepository;
+
+    protected SchoolService schoolService;
+    protected UserService userService;
+    protected UserSecurityService userSecurityService;
+
+    protected SchoolTestHelper schoolTestHelper;
+    protected UserTestHelper userTestHelper;
+    protected School school;
+
+    private boolean prepared;
+
+    protected void prepareUserServices() {
+        if (prepared) return;
+        prepared = true;
+
+        this.schoolRepository.deleteAll();
+        this.userRepository.deleteAll();
+        this.schoolService = new SchoolService(schoolRepository);
+        this.userService = new UserService(schoolRepository, userRepository);
+        this.userSecurityService = new UserSecurityService(userRepository);
+
+        this.userTestHelper = new UserTestHelper(userService, NoOpPasswordEncoder.getInstance());
+        this.schoolTestHelper = new SchoolTestHelper(schoolService);
+        this.school = this.schoolTestHelper.createSchool("테스트 학교", "서울");
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
